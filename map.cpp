f563201807e9f668e19b872714b2e36dfb707e94
gmwilli5 <gmwilli5@ncsu.edu>
2013-08-15 16:47:31 -0400

    renders a map full of squares cool line pattern

--- map.cpp
+++ map.cpp
@@ -3,6 +3,14 @@
 map_class::map_class()
 {
     tile=SDL_LoadBMP("enemy.bmp");
+    vec2 temp;
+    for(int iii=0;iii<600;iii+=30){
+        for(int ooo=0;ooo<700;ooo+=30){
+            temp.x=iii;
+            temp.y=ooo;
+            positions.push_back(temp);
+        }
+    }
 }
 SDL_Surface* map_class::get_tile()
 {
@@ -10,5 +18,8 @@ SDL_Surface* map_class::get_tile()
 }
 void map_class::render(SDL_Surface* screen)
 {
-    apply_surface(0,0,tile,screen);
+    //apply_surface(0,0,tile,screen);
+    for(unsigned int iii=0;iii<positions.size();iii++){
+        apply_surface(positions[iii].x,positions[iii].y,tile,screen);
+    }
 }
--- map.h
+++ map.h
@@ -1,10 +1,12 @@
 #ifndef MAP_H_INCLUDED
 #define MAP_H_INCLUDED
 #include "SDL/SDL.h"
+#include <vector>
+#include "structs.h"
 class map_class{
     private:
         SDL_Surface* tile;
-
+        std::vector<vec2> positions;
     public:
         map_class();
         SDL_Surface* get_tile();
