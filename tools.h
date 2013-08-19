3dc4e25db1f5602443c79aa9e12dc7eb0da36b46
gmwilli5 <gmwilli5@ncsu.edu>
2013-08-18 22:19:53 -0400

    small changes

---
+++ tools.h
@@ -0,0 +1,16 @@
+#ifndef TOOLS_H_INCLUDED
+#define TOOLS_H_INCLUDED
+vec2 operator+(vec2 a,vec2 b);
+vec2 operator+=(vec2& a,vec2 b);
+vec2 dot_product(vec2 a,vec2 b);
+aabb construct_aabb(vec2 vec,int width,int height);
+aabb construct_aabb(vec3 vec,int width,int height);
+bool vec2_vs_vec3(vec2 a,vec3 b);
+bool aabb_vs_aabb(aabb vec_a, aabb vec_b);
+bool vec2_vs_vec2(vec2 vec_a,vec2 vec_b);
+bool detect_collision(aabb a, aabb b);
+bool detect_collision(vec2 a,vec3 b);
+bool detect_collision(vec2 a,vec2 b);
+void zero_vec2(vec2& a);
+void zero_vec2_y(vec2& a);
+#endif // TOOLS_H_INCLUDED
